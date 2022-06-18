1cdbbf84469100186dc12f766d4c5f6225191086
kenko <universeengineer@gmail.com>
2022-06-19 07:28:54 +0900

    :ambulance: fix curl url

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ DOTPATH=~/.dotfiles
 if has "git"; then
   git clone --recursive https://github.com/44103/dotfiles.git "$DOTPATH"
   elif has "curl" || has "wget"; then
-    tarball="https://github.com/b4b4r07/dotfiles/archive/master.tar.gz"
+    tarball="https://github.com/44103/dotfiles/archive/main.tar.gz"
     if has "curl"; then
       curl -L "$tarball"
     elif has "wget"; then
